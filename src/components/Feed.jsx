5404c80f406e588ad5b44b15715abf0a604a0c1c
raghav161 <raghavbansal242003@gmail.com>
2024-07-18 16:46:59 +0530

    added github link

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Box, Stack, Typography } from "@mui/material";
-
+import { Box, Stack, Typography, Link } from "@mui/material";
+import GitHubIcon from '@mui/icons-material/GitHub';
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 import { Videos, Sidebar } from "./";
-
 const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState("New");
   const [videos, setVideos] = useState([]);
@@ -21,12 +20,14 @@ const Feed = () => {
         <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
-        <span sx={{ margin: '0 8px' }}>Raghav Bansal </span>
-          Copyright © 2024
+          <Link href="https://github.com/raghav161/" target="_blank" style={{color: 'white', display: 'flex', justifyContent: 'center'}}>
+            <GitHubIcon style={{marginRight: '12px', marginTop: '-4px'}}/>
+            Raghav Bansal 
+          </Link>
         </Typography>
       </Box>
 
-      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Box p={5} sx={{ overflowY: "auto", height: "90vh", flex: 2, marginLeft: '50px' }}>
         <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
           {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
         </Typography>
